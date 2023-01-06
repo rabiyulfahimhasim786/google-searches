c4f1cd31bd20fa8cebf6b29cab857ca6261a8971
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-01-06 07:46:23 -0600

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('fileuploading/', views.uploadings, name='uploadings'),
     path('form/', views.form_upload, name='form_upload'),
     path('csv/', views.csvs, name='csvs'),
+    path('csv/<int:id>/',views.delete_document,name='delete_document'),
     #path('new/', views.newest, name='newest'),
 ]
